779fd0a47c1f2fbcf332e137da523e0218a65e85
adityapatil <aspatik9021@gmail.com>
2025-07-06 20:13:19 +0530

    Mongo db setup added

--- src/test/kotlin/com/itpm/demoapi/demo/DemoApiApplicationTests.kt
+++ src/test/kotlin/com/itpm/demoapi/demo/DemoApiApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoapiApplicationTests {
+class DemoApiApplicationTests {
 
 	@Test
 	fun contextLoads() {
